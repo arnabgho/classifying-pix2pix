340ee0d827db323c2d1d73e7a694a25eee370f39
arnabghosh93@gmail.com <arnabg@ttic.edu>
2017-02-08 15:51:53 +0000

    Classifying Discriminator implemented in the conditional setting

--- models.lua
+++ models.lua
@@ -212,3 +212,39 @@ function defineD_n_layers(input_nc, output_nc, ndf, n_layers)
         return netD
     end
 end
+
+function defineD_n_layers_ngen(input_nc, output_nc, ndf, n_layers,ngen)
+    if n_layers==0 then
+        return defineD_pixelGAN(input_nc, output_nc, ndf)
+    else
+    
+        local netD = nn.Sequential()
+        
+        -- input is (nc) x 256 x 256
+        netD:add(nn.SpatialConvolution(input_nc+output_nc, ndf, 4, 4, 2, 2, 1, 1))
+        netD:add(nn.LeakyReLU(0.2, true))
+        
+        local nf_mult = 1
+        local nf_mult_prev = 1
+        for n = 1, n_layers-1 do 
+            nf_mult_prev = nf_mult
+            nf_mult = math.min(2^n,8)
+            netD:add(nn.SpatialConvolution(ndf * nf_mult_prev, ndf * nf_mult, 4, 4, 2, 2, 1, 1))
+            netD:add(nn.SpatialBatchNormalization(ndf * nf_mult)):add(nn.LeakyReLU(0.2, true))
+        end
+        
+        -- state size: (ndf*M) x N x N
+        nf_mult_prev = nf_mult
+        nf_mult = math.min(2^n_layers,8)
+        netD:add(nn.SpatialConvolution(ndf * nf_mult_prev, ndf * nf_mult, 4, 4, 1, 1, 1, 1))
+        netD:add(nn.SpatialBatchNormalization(ndf * nf_mult)):add(nn.LeakyReLU(0.2, true))
+        -- state size: (ndf*M*2) x (N-1) x (N-1)
+        netD:add(nn.SpatialConvolution(ndf * nf_mult, ngen , 4, 4, 1, 1, 1, 1))
+        -- state size: 1 x (N-2) x (N-2)
+        
+        netD:add(nn.Sigmoid())
+        -- state size: 1 x (N-2) x (N-2)
+        
+        return netD
+    end
+end
